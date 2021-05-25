a28daf471618eaf2316ae18082a55a0ccc88f5c6
Sourabh Mishra <Sourabh19278@iiitd.ac.in>
2021-05-25 13:03:38 +0530

    Updated test files

--- Solidity Files/Testing/test/EvenNoChecker.js
+++ Solidity Files/Testing/test/EvenNoChecker.js
@@ -4,11 +4,19 @@ const isEven = require("../scripts/EvenNoChecker.js");
 
 
 describe("Checking isEven(num)",()=>{
-	it("Should return true for even",()=>{
-       expect(isEven(2)).to.be.true;
-	});
-	it("Should return false for odd",()=>{
-      expect(isEven(3)).to.be.false;
-	});
+
+       it("Should return true for even", async ()=>{
+       	   const result = await isEven(2);
+       	   expect(result).to.be.true;
+
+       });
+
+       it("Should return false for odd",async()=>{
+           const result  = await isEven(3);
+           expect(result).to.be.false;
+       });
+
+
 
 });
+
