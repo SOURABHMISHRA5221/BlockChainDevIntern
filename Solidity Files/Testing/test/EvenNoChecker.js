6be69685062a99e6a60d18f66ce1d5f5828547f1
Sourabh Mishra <Sourabh19278@iiitd.ac.in>
2021-05-25 14:13:15 +0530

    Added sinon

--- Solidity Files/Testing/test/EvenNoChecker.js
+++ Solidity Files/Testing/test/EvenNoChecker.js
@@ -8,15 +8,12 @@ describe("Checking isEven(num)",()=>{
        it("Should return true for even", async ()=>{
        	   const result = await isEven(2);
        	   expect(result).to.be.true;
-
        });
 
        it("Should return false for odd",async()=>{
            const result  = await isEven(3);
            expect(result).to.be.false;
        });
-
-
 
 });
 
